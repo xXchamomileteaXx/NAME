1b21483a4aab8b922931db9f221f89159a30ac9a
xXchamomileteaXx <71933414+xXchamomileteaXx@users.noreply.github.com>
2021-02-26 10:54:57 +0300

    Add files via upload

---
+++ 2.1.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+struct Advertising
+{
+    int watch;
+    double pr;
+    double money;
+};
+
+int main()
+{
+    Advertising adv_web;
+    int n = 0, check = 0;
+    cin >> adv_web.watch >> adv_web.pr >> adv_web.money;
+    double income = adv_web.watch * (adv_web.pr / 100) * adv_web.money;
+    double dub = income * 10;
+
+
+    if (adv_web.pr < 0 || adv_web.pr > 100 || adv_web.watch < 0 || adv_web.money <= 0 || adv_web.watch > 20)
+    {
+        cout << "Invalid input";
+        check = 1;
+    }
+
+    if (income == 0)
+    {
+        cout << "0.0";
+        check = 1;
+    }
+
+    if (income > 0 && income < 1)
+    {
+        while (dub < 1)
+        {
+            n++;
+            dub *= 10;
+        }
+
+        if ((int)dub < 5) n++;
+        cout << fixed << setprecision(n) << income;
+        check = 1;
+    }
+
+    if (check == 0)
+    {
+        cout << round(income * 100.0) / 100.0;
+        check = 1;
+    }
+
+    return 0;
+}
